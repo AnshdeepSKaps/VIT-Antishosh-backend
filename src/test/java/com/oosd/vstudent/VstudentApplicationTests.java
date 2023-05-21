239445e35d53811e6458a858b0b254fd470fa2d9
anshdeep <kapulaanshdeep@gmail.com>
2023-05-22 00:09:50 +0530

    some fixes

--- src/test/java/com/oosd/vstudent/VstudentApplicationTests.java
+++ src/test/java/com/oosd/vstudent/VstudentApplicationTests.java
@@ -3,7 +3,7 @@ package com.oosd.vstudent;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = VstudentApplication.class)
 class VstudentApplicationTests {
 
     @Test
